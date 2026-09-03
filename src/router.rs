35503bbe5574d290e924dcbfa9be2c21afba0291
Voyadjer <voyadjernetherlands@gmail.com>
2026-09-03 19:35:24 +0300

    feat(ANY-12): Add owned tags endpoint

--- src/router.rs
+++ src/router.rs
@@ -19,6 +19,7 @@ pub fn create_router(state: AppState) -> Router {
         .route("/posts", get(posts::list_posts))
         .route("/users/me/posts", get(posts::list_owned_posts))
         .route("/tags", get(tags::list_tags))
+        .route("/users/me/tags", get(tags::list_owned_tags))
         .route("/users", post(users::create_user))
         .route("/auth/login", post(auth::login_user))
         .route("/users/me", get(auth::get_current_user))
